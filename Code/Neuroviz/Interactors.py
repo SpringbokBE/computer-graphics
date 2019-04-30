bbd0945acba6bde87f77744c62d9a1e64bfa196c
Gerbrand De Laender <gdlaende@github.ugent.be>
2019-04-30 23:04:32 +0200

    Added DSA widget.

--- Code/Neuroviz/Interactors.py
+++ Code/Neuroviz/Interactors.py
@@ -1,9 +1,10 @@
 from logging import getLogger
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QGroupBox,
-                             QLabel, QSizePolicy, QSlider, QSpacerItem,
-                             QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox,
+                             QDoubleSpinBox, QGridLayout, QGroupBox, QLabel,
+                             QSizePolicy, QSlider, QSpacerItem, QVBoxLayout,
+                             QWidget)
 
 from Neuroviz.UiComponents import SliderGroup
 
@@ -140,6 +141,73 @@ class EEGWidget( QWidget ):
         verticalLayout.addWidget( self._groupBoxAnimations )
         verticalLayout.addItem( self._spacerItem )
         self.setLayout( verticalLayout )
+
+################################################################################
+################################################################################
+
+class DSAWidget( QWidget ):
+
+    ############################################################################
+
+    def __init__( self, dockWidget, *args, **kwargs ):
+        """
+        """
+        logger.info( f"Creating {__class__.__name__}..." )
+
+        self._dockWidget = dockWidget
+        self._settings = QApplication.instance().settings
+
+        super().__init__( *args, **kwargs )
+
+        self._createLayout()
+
+    ############################################################################
+
+    def activate( self ):
+        """
+        Activate the interactor widget.
+        """
+        self._dockWidget.setWidget( self )
+
+    ############################################################################
+
+    def _createLayout( self ):
+        """
+        """
+        self._labelDataSet = QLabel( "Dataset", self )
+        self.comboBoxDataSet = QComboBox( self )
+
+        self._labelHueMultiplier = QLabel( "Color range", self )
+        self.spinBoxHueMultiplier = QDoubleSpinBox( self )
+        self.spinBoxHueMultiplier.setRange( 0.5, 1.5 )
+        self.spinBoxHueMultiplier.setDecimals( 2 )
+        self.spinBoxHueMultiplier.setSingleStep( 0.01 )
+
+        self._labelHueConstant = QLabel( "Color offset", self )
+        self.spinBoxHueConstant = QDoubleSpinBox( self )
+        self.spinBoxHueConstant.setRange( 0.0, 1.0 )
+        self.spinBoxHueConstant.setDecimals( 2 )
+        self.spinBoxHueConstant.setSingleStep( 0.01 )
+
+        self._labelValueMultiplier = QLabel( "Detail", self )
+        self.spinBoxValueMultiplier = QDoubleSpinBox( self )
+        self.spinBoxValueMultiplier.setRange( 0.5, 10.0 )
+        self.spinBoxValueMultiplier.setDecimals( 2 )
+        self.spinBoxValueMultiplier.setSingleStep( 0.01 )
+
+        self._spacerItem = QSpacerItem( 20, 40, QSizePolicy.Expanding, QSizePolicy.Expanding )
+
+        gridLayout = QGridLayout()
+        gridLayout.addWidget( self._labelDataSet, 0, 0, 1, 2 )
+        gridLayout.addWidget( self.comboBoxDataSet, 1, 0, 1, 2 )
+        gridLayout.addWidget( self._labelHueMultiplier, 2, 0, 1, 1 )
+        gridLayout.addWidget( self.spinBoxHueMultiplier, 2, 1, 1, 1 )
+        gridLayout.addWidget( self._labelHueConstant, 3, 0, 1, 1 )
+        gridLayout.addWidget( self.spinBoxHueConstant, 3, 1, 1, 1 )
+        gridLayout.addWidget( self._labelValueMultiplier, 4, 0, 1, 1 )
+        gridLayout.addWidget( self.spinBoxValueMultiplier, 4, 1, 1, 1 )
+        gridLayout.addItem( self._spacerItem )
+        self.setLayout( gridLayout )
 
 ################################################################################
 ################################################################################
